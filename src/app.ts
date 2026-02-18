622971e38b9d14a13df0dc87f9509509b0ac09f6
Marième Ndiaye <ndiayemereme5@gmail.com>
2026-02-18 20:23:55 +0000

    corriger swagger

--- src/app.ts
+++ src/app.ts
@@ -11,9 +11,9 @@ export const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use("/api/auth", authRouter)
 app.use("/api/classes", classeRouter)
 app.use("/api/cours", coursRouter)
 app.use("/api/etudiants", etudiantRouter)
 app.use("/api/stats", statsRouter)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
